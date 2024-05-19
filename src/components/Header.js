fe67cfddeed2e2355e2eac30b37cdf5f471857d9
Johnny Hall <jbhall4291@gmail.com>
2024-05-19 22:44:35 +0100

    add navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,14 @@
+import Link from "next/link";
+
 export default function Header() {
     return (
-        <div className="bg-purple-500">
-            hello from header
-        </div>
+        <header className="bg-purple-500 navbar px-4">
+            <Link href="/" className="navbar-start">LOGO</Link>
+
+            <div className="navbar-end flex flex-row gap-x-8">
+                <Link href="about">About</Link>
+                <Link href="skills">Skills</Link>
+            </div>
+        </header>
     );
 }
