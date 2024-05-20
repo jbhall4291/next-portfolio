60559b6cecec6aa877a6049bd03e092bc544d1c0
Johnny Hall <jbhall4291@gmail.com>
2024-05-20 22:01:43 +0100

    start populating home page, add images

--- src/app/page.js
+++ src/app/page.js
@@ -5,10 +5,25 @@ export default function Home() {
   return (
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content flex-col lg:flex-row-reverse">
-        <img src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" className="max-w-sm rounded-lg shadow-2xl" />
+        <Image
+          src="/images/hero-desktop.jpg"
+          width={500}
+          height={500}
+          className="hidden md:block rounded-lg"
+          alt="headshot of johnny hall"
+        />
+        <Image
+          src="/images/hero-mobile.jpg"
+          width={400}
+          height={400}
+          className="block md:hidden rounded-lg"
+          alt="headshot of johnny hall"
+        />
         <div>
-          <h1 className="text-5xl font-bold">Box Office News!</h1>
-          <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+          <h1 className="text-5xl font-bold">Hi, I'm Johnny.</h1>
+          <p className="py-6">Full-stack software developer by day, and a devoted pizza enthusiast by night.
+            I love building cool stuff and learning new technologies.
+            Whether it's front-end or back-end, I'm always up for a challenge 🚀.</p>
           <button className="btn btn-primary">Get Started</button>
         </div>
       </div>
