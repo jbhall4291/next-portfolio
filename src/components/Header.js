0b1f41f982c3d8274ccdb20ce502b97d847c8c48
Johnny <jbhall4291@gmail.com>
2024-08-31 16:57:07 +0100

    tweak layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 export default function Header() {
     return (
-        <div className="navbar bg-base-100 lg:px-10 text-xl">
-            <div className="navbar-start">
+        <div className="navbar bg-base-100 lg:px-10 text-xl text-white">
+            <div className="w-full md:navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
@@ -15,12 +15,10 @@ export default function Header() {
                         <Link href="about">Contact</Link>
                     </ul>
                 </div>
-
-                <Link className="" href="/">Johnny Hall : Software Developer</Link>
-
+                <Link className="w-full text-center md:text-left text-[1.1rem] lg:text-[1.3rem] " href="/">Johnny Hall : Software Developer</Link>
             </div>
-            <div className="navbar-end hidden lg:flex ">
-                <ul className="menu menu-horizontal lg:flex gap-x-10">
+            <div className="navbar-end hidden md:flex">
+                <ul className="menu menu-horizontal lg:flex gap-x-10 text-[1.1rem] lg:text-[1.3rem]">
                     <Link href="skills">Skills</Link>
                     <Link href="projects">Projects</Link>
                     <Link href="about">About</Link>
