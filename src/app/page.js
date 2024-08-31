477cce981ba8dbd28261203bdcba779c5fd917c3
Johnny <jbhall4291@gmail.com>
2024-08-31 16:57:31 +0100

    tweak style and layout

--- src/app/page.js
+++ src/app/page.js
@@ -7,46 +7,49 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHandshake, faCircleRight } from '@fortawesome/free-solid-svg-icons'
 
 export default function Home() {
+
+  var notationPrimaryColour = "#F51767"
   return (
-    <div className="hero min-h-[calc(100vh-64px)] bg-base-200">
+    <div className="hero min-h-[calc(100vh-64px)] bg-white">
       <div className="hero-content flex-col lg:flex-row-reverse">
         <Image
-          src="/images/hero-desktop.jpg"
-          width={460}
-          height={460}
-          className="hidden md:block rounded-lg"
-          alt="headshot of johnny hall"
-          loading="eager"
-          priority={true}
-        />
-        <Image
-          src="/images/hero-mobile.jpg"
-          width={400}
-          height={400}
+          src="/images/profilepic-white-square.png"
+          width={300}
+          height={300}
           className="block md:hidden rounded-lg"
           alt="headshot of johnny hall"
           loading="eager"
           priority={true}
         />
+        <Image
+          src="/images/profilepic-white-square.png"
+          width={450}
+          height={450}
+          className="hidden md:block "
+          alt="headshot of johnny hall"
+          loading="eager"
+          priority={true}
+        />
+
         <div>
-          <h1 className="text-5xl font-bold text-primary">Hi, I&apos;m Johnny.</h1>
-          <p className="py-6">Full-stack software developer by day, and a devoted pizza enthusiast{" "}
+          <h1 className="text-4xl md:text-6xl font-bold text-primary">Hi, I&apos;m Johnny.</h1>
+          <h2 className="pt-4 pb-6 md:py-6 text-lg text-[#1f232a]">Full-stack software developer by day, and a devoted pizza enthusiast{" "}
             <RoughNotation
-              color="#fe003a"
+              color={notationPrimaryColour}
               strokeWidth="5"
               type="crossed-off"
               show={true}
               animationDelay="1500"
               multiline="true"
             >
-              by night
+              by night.
             </RoughNotation>
-            .<br />
+            <br />
 
 
 
             <RoughNotation
-              color="#fe003a"
+              color={notationPrimaryColour}
               strokeWidth="5"
               type="underline"
               show={true}
@@ -65,25 +68,26 @@ export default function Home() {
               strokeWidth="5"
               show={true}
               animationDelay="4500"
-              color="#fe003a"
+              color={notationPrimaryColour}
               animationDuration="1200"
             >
               up for a challenge 🚀
             </RoughNotation>
-          </p>
+          </h2>
 
 
 
-          <div className="flex flex-row gap-x-10  animate-[wiggle_1s_ease-in-out_infinite]">
+          <div className="flex flex-col md:flex-row gap-x-8 gap-y-4">
 
-            <button className="btn transition-all duration-700 bg-primary border-2 border-primary text-white hover:border-primary  hover:bg-primary text-1xl  hover:shadow-[0_0px_15px_rgba(254,_0,_58,_1)] ">Get in touch
+            <button className="font-semibold text-lg btn rounded-lg transition-all duration-700 bg-primary border-2 border-primary text-white hover:border-primary  hover:bg-primary text-1xl  hover:shadow-[0_0px_15px_rgba(254,_0,_58,_1)] ">Get in touch
               <FontAwesomeIcon icon={faHandshake} className="fa-regular fa-xl" />
             </button>
-            <button className="btn  transition-all duration-700 text-primary border-primary border-2  bg-white hover:bg-white hover:text-primary hover:border-primary  hover:shadow-[0_0px_15px_rgba(254,_0,_58,_1)] ">See my projects
+
+
+            <button className="font-semibold text-lg btn rounded-lg transition-all duration-700 text-primary border-primary border-2  bg-white hover:bg-white hover:text-primary hover:border-primary  hover:shadow-[0_0px_15px_rgba(254,_0,_58,_1)] ">
+              See my projects
               <FontAwesomeIcon icon={faCircleRight} className="fa-regular  fa-xl" />
             </button>
-
-
 
 
           </div>
