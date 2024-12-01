5ee899efecfb961475a2d0dc203accd69b2b338c
Johnny <jbhall4291@gmail.com>
2024-12-01 19:43:13 +0000

    fix link paths

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,20 +9,20 @@ export default function Header() {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </div>
                     <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                        <Link href="skills">Skills</Link>
-                        <Link href="skills">Projects</Link>
-                        <Link href="about">About</Link>
-                        <Link href="about">Contact</Link>
+                        <Link href="/skills">Skills</Link>
+                        <Link href="/skills">Projects</Link>
+                        <Link href="/about">About</Link>
+                        <Link href="/contact">Contact</Link>
                     </ul>
                 </div>
                 <Link className="w-full text-center md:text-left text-[1.1rem] lg:text-[1.3rem] " href="/">Johnny Hall : Software Developer</Link>
             </div>
             <div className="navbar-end hidden md:flex">
                 <ul className="menu menu-horizontal lg:flex gap-x-10 text-[1.1rem] lg:text-[1.3rem]">
-                    <Link href="skills">Skills</Link>
-                    <Link href="projects">Projects</Link>
-                    <Link href="about">About</Link>
-                    <Link href="contact">Contact</Link>
+                    <Link href="/skills">Skills</Link>
+                    <Link href="/projects">Projects</Link>
+                    <Link href="/about">About</Link>
+                    <Link href="/contact">Contact</Link>
                 </ul>
             </div>
             {/* <div className="navbar-end">
