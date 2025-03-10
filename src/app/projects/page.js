4130d581a2e28b8e4e06629da738a2d5f021eb5a
Johnny Hall <jbhall4291@gmail.com>
2025-03-10 16:49:10 +0000

    update 'video demo' link

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -61,7 +61,13 @@ export default function Projects() {
                         <h2 className="card-title">Fan Finder</h2>
                         <p>A location-based social networking app for iOS & Android, built with React Native.</p>
                         <div className="card-actions justify-between mt-4">
-                            <button className="btn btn-primary">Watch Video Demo</button>
+
+
+                            <a href="https://www.youtube.com/watch?v=kQ7weiOZzHM&ab_channel=JHall" target="_blank">
+                                <button className="btn btn-primary">Watch Video Demo</button>
+                            </a>
+
+
                             <a href="/projects/fanfinder">
                                 <button className="btn btn-secondary">Learn More
                                     <svg width="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zm395.3 11.3l-112 112c-4.6 4.6-11.5 5.9-17.4 3.5s-9.9-8.3-9.9-14.8l0-64-96 0c-17.7 0-32-14.3-32-32l0-32c0-17.7 14.3-32 32-32l96 0 0-64c0-6.5 3.9-12.3 9.9-14.8s12.9-1.1 17.4 3.5l112 112c6.2 6.2 6.2 16.4 0 22.6z" /></svg>
