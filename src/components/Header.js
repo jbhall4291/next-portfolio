4757679426aa2914ac32e1a5c2b145d550d0f265
Johnny Hall <jbhall4291@gmail.com>
2025-03-10 18:54:03 +0000

    tweak mobile nav menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,12 +8,12 @@ export default function Header() {
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </div>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                        <Link href="/" >Home</Link>
-                        <Link href="/skills">Skills</Link>
-                        <Link href="/skills">Projects</Link>
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 text-2xl">
+                        <Link href="/" className="hover:text-primary">Home</Link>
+                        <Link href="/skills" className="hover:text-primary">Skills</Link>
+                        <Link href="/skills" className="hover:text-primary">Projects</Link>
                         {/* <Link href="/about">About</Link> */}
-                        <Link href="/contact">Contact</Link>
+                        <Link href="/contact" className="hover:text-primary">Contact</Link>
                     </ul>
                 </div>
                 <Link className="text-center md:text-left text-[1.1rem] lg:text-[1.3rem] hover:text-primary" href="/">{'{ johnny hall : software developer }'}</Link>
