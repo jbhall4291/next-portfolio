4f55d654191039c8c0d28d8ce73e8ca818fb21db
Johnny Hall <jbhall4291@gmail.com>
2025-03-10 19:16:49 +0000

    tweak order of cards

--- src/app/skills/page.js
+++ src/app/skills/page.js
@@ -33,14 +33,14 @@ export default function Skills() {
                     <div className="flex flex-col justify-center items-center ">
                         <div className="text-left w-fit pb-2">⚙️ Languages & Frameworks</div>
                         <div className="flex flex-row flex-wrap  gap-5 justify-center">
+                            <SkillCard label="React" icon="devicon-react-original colored" />
+                            <SkillCard label="Next.js" icon="devicon-nextjs-plain colored" />
                             <SkillCard label="C#" icon="devicon-csharp-plain colored" />
                             <SkillCard label=".NET" icon="devicon-dot-net-plain colored" />
-                            <SkillCard label="React" icon="devicon-react-original colored" />
+                            <SkillCard label="Vue.js" icon="devicon-vuejs-plain colored" />
+                            <SkillCard label="Express.js" icon="devicon-express-original colored" />
                             <SkillCard label="JavaScript" icon="devicon-javascript-plain colored" />
                             <SkillCard label="Node.js" icon="devicon-nodejs-plain colored" />
-                            <SkillCard label="Next.js" icon="devicon-nextjs-plain colored" />
-                            <SkillCard label="Express.js" icon="devicon-express-original colored" />
-                            <SkillCard label="Vue.js" icon="devicon-vuejs-plain colored" />
                         </div>
                     </div>
 
@@ -48,10 +48,10 @@ export default function Skills() {
                     <div className="flex flex-col justify-center items-center ">
                         <div className="text-left w-fit pb-2">🗄️ Databases</div>
                         <div className="flex flex-row flex-wrap  gap-5 justify-center w-fit">
-                            <SkillCard label="Firebase" icon="devicon-firebase-plain colored" />
-                            <SkillCard label="PostgreSQL" icon="devicon-postgresql-plain colored" />
-                            <SkillCard label="MongoDB" icon="devicon-mongodb-plain colored" />
                             <SkillCard label="SQL Server" icon="devicon-microsoftsqlserver-plain colored" />
+                            <SkillCard label="PostgreSQL" icon="devicon-postgresql-plain colored" />
+                            <SkillCard label="Firebase" icon="devicon-firebase-plain colored" />
+                            <SkillCard label="MongoDB" icon="devicon-mongodb-plain colored" />
                         </div>
                     </div>
 
@@ -69,12 +69,12 @@ export default function Skills() {
                         <div className="flex flex-row flex-wrap  gap-5 justify-center w-fit">
                             <SkillCard label="Tailwind" icon="devicon-tailwindcss-plain colored" />
                             <SkillCard label="Bootstrap" icon="devicon-bootstrap-plain colored" />
-                            <SkillCard label="HTML5" icon="devicon devicon-html5-plain colored" />
-                            <SkillCard label="CSS3" icon="devicon devicon-css3-plain colored" />
                             <SkillCard
                                 label="Figma"
                                 icon="devicon-figma-plain colored text-2xl md:text-5xl "
                             />
+                            <SkillCard label="HTML5" icon="devicon devicon-html5-plain colored" />
+                            <SkillCard label="CSS3" icon="devicon devicon-css3-plain colored" />
                         </div>
                     </div>
                 </div>
