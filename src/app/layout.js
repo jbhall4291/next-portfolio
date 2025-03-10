578c7f76928a2b96bc77bdf08aaaf99794d596d2
Johnny Hall <jbhall4291@gmail.com>
2025-03-10 16:30:24 +0000

    tweak navbar content and style, add favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,6 +21,16 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
+        <link rel="icon" href="/favicon.ico" sizes="32x32" />
+
+
+        <link rel="apple-touch-icon" href="/apple-icon-180x180.png" />
+
+
+        <link rel="icon" href="/android-icon-192x192.png" sizes="192x192" />
+
+
+        <link rel="icon" href="/favicon-512x512.png" sizes="512x512" />
         <link rel="stylesheet" type='text/css' href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css" />
       </Head>
 
